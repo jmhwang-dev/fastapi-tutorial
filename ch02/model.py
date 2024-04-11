ea12e87ed22489be725a5fc3171ffbc2dba2470c
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-11 15:51:37 +0900

    중첩모델 적용

--- ch02/model.py
+++ ch02/model.py
@@ -1,5 +1,9 @@
+# 파이썬의 타입 어노테이션을 사용해서 데이터를 검증하는 파이썬 라이브러리
 from pydantic import BaseModel
 
+class Item(BaseModel):
+    item: str
+    status: str
 class Todo(BaseModel):
     id: int
-    item: str
+    item: Item
