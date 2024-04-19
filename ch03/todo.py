3d7313798725e3abf30fad71c1f8fc67fc6b909b
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-19 21:31:59 +0900

    Path: 경로 매개변수 remind
    - 라우트 함수에 있는 다른 인수와 경로 매개변수를 구분하는 역할
    - 첫 번째 인수가 '...' 이면 경로 매개변수를 반드시 지정해야한다. None 이나 기본값을 설정하면 선택적 매개변수가 된다.
    - 보통 url을 구성하기 위해선 필수여야한다.
    - 유효성을 추가할 수 있다. ex) ge, le

--- ch03/todo.py
+++ ch03/todo.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 todo_list = []
+
+@todo_router.get('/todo/{todo_id}')
+async def get_single_todo(todo_id: int = Path(..., title="ReDoc에만 나와요", description="ReDoc, Swagger 둘다 나와요")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
 
 @todo_router.get("/todo", response_model=TodoItems)
 async def retrive_todo() -> dict:
