76dab5276688184707a96533003d37ce4d943650
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-05-24 15:00:21 +0900

    세팅 수정

--- ch04/run.sh
+++ ch04/run.sh
@@ -1,7 +1,7 @@
 # conda activate fastapi
 pip install -r ../requirements.txt
 
-mkdir template
+mkdir -p template
 touch {home,todo}.html
 
-cp ../ch03/todo.py .
+cp ../ch03/todo.py ../ch03/model.py ../ch03/api.py .
