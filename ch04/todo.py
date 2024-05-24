d8fb3bf3be5c39d99843d8a1e749b987b0040f37
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-05-24 15:40:09 +0900

    GET 라우트에 템플릿 적용

--- ch04/todo.py
+++ ch04/todo.py
@@ -1,26 +1,32 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Request, Depends
+from fastapi.templating import Jinja2Templates
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 todo_list = []
 
+templates = Jinja2Templates(directory="templates/")
+
 @todo_router.get('/todo/{todo_id}')
-async def get_single_todo(todo_id: int = Path(..., title="ReDoc에만 나와요", description="ReDoc, Swagger 둘다 나와요")):
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="ReDoc에만 나와요", description="ReDoc, Swagger 둘다 나와요")):
     for todo in todo_list:
         if todo.id == todo_id:
-            return {
+            return templates.TemplateResponse("todo.html", {
+                "request": request,
                 "todo": todo
-            }
+            })
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Todo with supplied ID doesn't exist."
     )
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrive_todo() -> dict:
-    return {
+async def retrive_todo(request: Request) -> dict:
+    return templates.TemplateResponse("todo.html", {
+        "request": request,
         "todos": todo_list
-    }
+    })
+    
 
 @todo_router.post('/todo', status_code=201)
 async def add_todo(todo: Todo) -> dict:
