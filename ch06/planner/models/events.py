f7d8da597a846556a9f369bcf5ae1a8942e092f3
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-05-28 16:23:07 +0900

    SQLModel 테이블 클래스 생성

--- ch06/planner/models/events.py
+++ ch06/planner/models/events.py
@@ -1,21 +1,40 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import SQLModel, Field, Column, JSON
+from typing import List, Optional
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
 
-    # 문서화시 샘플 데이터 보여주기 용도
     class Config:
+        arbitrary_types_allowed = True
         json_schema_extra = {
             "example": {
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "We will be discussing the contents fo the FastAPi book in this event. Ensure tot come with your own copy to win gifts!",
+                "description": "We will be discussing the contents of the FastAPi book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str] 
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPi book in this event. Ensure to come with your own copy to win gifts!",
                 "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet"
             }
